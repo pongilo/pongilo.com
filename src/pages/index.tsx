58b94a68af57f7cd401ce60a2c4f3b980374c703
Elton Pongilo <pongilo@Eltons-MacBook-Air.local>
2021-12-30 09:01:21 -0300

    change the content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <h1 className="font-sans font-bold text-[2rem] sm:text-[3rem] md:text-[3.5rem] lg:text-[4rem] leading-[1.3]">
           <div>
             <span className="animate-fade-in-1 opacity-0">Elton Pongilo</span>{' '}
-            <span className="animate-fade-in-5 opacity-0 font-serif font-normal text-zinc-400">
+            <span className="animate-fade-in-6 opacity-0 font-serif font-normal text-zinc-400">
               is a designer and web developer
             </span>{' '}
             <span className="animate-fade-in-2 opacity-0">
@@ -26,18 +26,22 @@ export default function Home() {
             </span>
           </div>
           <div className="my-8 sm:my-16">
-            <span className="animate-fade-in-3 opacity-0">
-              I make cool and expressive things
-            </span>{' '}
-            <span className="animate-fade-in-5 opacity-0 font-serif font-normal text-zinc-400">
-              for the internet. Check my
+            <span className="animate-fade-in-3 opacity-0">I make</span>{' '}
+            <span className="animate-fade-in-6 opacity-0 font-serif font-normal text-zinc-400">
+              cool and expressive
             </span>{' '}
             <span className="animate-fade-in-4 opacity-0">
-              select works below.
+              things for the internet.
+            </span>{' '}
+            <span className="animate-fade-in-6 opacity-0 font-serif font-normal text-zinc-400">
+              Check my works on
+            </span>{' '}
+            <span className="animate-fade-in-5 opacity-0">
+              my social media below.
             </span>
           </div>
         </h1>
-        <div className="animate-fade-in-5 opacity-0 mb-8 sm:mb-16 flex flex-col flex-nowrap font-serif font-normal italic text-zinc-400 text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.5rem] leading-[1]">
+        <div className="animate-fade-in-6 opacity-0 mb-8 sm:mb-16 flex flex-col flex-nowrap font-serif font-normal italic text-zinc-400 text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.5rem] leading-[1]">
           <a
             href="https://github.com/pongilo"
             target="_blank"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       animation: {
-        'fade-in-1': 'fadeIn 1.5s linear .5s forwards',
-        'fade-in-2': 'fadeIn 1.5s linear 1s forwards',
-        'fade-in-3': 'fadeIn 1.5s linear 1.5s forwards',
-        'fade-in-4': 'fadeIn 1.5s linear 2s forwards',
-        'fade-in-5': 'fadeIn 1.5s linear 2.5s forwards',
+        'fade-in-1': 'fadeIn 1.5s linear .8s forwards',
+        'fade-in-2': 'fadeIn 1.5s linear 1.6s forwards',
+        'fade-in-3': 'fadeIn 1.5s linear 2.4s forwards',
+        'fade-in-4': 'fadeIn 1.5s linear 3.2s forwards',
+        'fade-in-5': 'fadeIn 1.5s linear 4s forwards',
+        'fade-in-6': 'fadeIn 1.5s linear 4.8s forwards',
       },
       keyframes: {
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
-        }
-      }
+        },
+      },
     },
   },
   plugins: [],
-}
+};
