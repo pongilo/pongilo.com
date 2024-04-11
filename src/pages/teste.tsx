7e833ee6c1d7ff474d0322bb88f53ed63b541856
Elton Pongilo <elton.pongilo@gmail.com>
2024-04-11 08:28:47 -0300

    teste

---
+++ src/pages/teste.tsx
@@ -0,0 +1,11 @@
+export default function Teste() {
+  function handleOnClick() {
+    window.navigator.vibrate(200);
+  }
+
+  return (
+    <button type="button" onClick={handleOnClick}>
+      Vibrar
+    </button>
+  )
+}
